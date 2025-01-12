0e772552582ed9d5d78688c7fded87441fe5bab5
kanechan25 <kt.itengineer@gmail.com>
2025-01-12 17:18:15 +0700

    fix fetch cex ticker

--- src/modules/cex-arb/cex_arb.service.ts
+++ src/modules/cex-arb/cex_arb.service.ts
@@ -1,21 +1,19 @@
-import * as ccxt from 'ccxt';
+import { IMultiTickers, ITicker, ITickerRecords } from '@/types/cex';
 import { Injectable, Logger, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PricesService } from '@/modules/cex/prices/prices.service';
+import * as ccxt from 'ccxt';
 import { BinanceService } from '@/modules/cex/binance/binance.service';
-import { OkxService } from '@/modules/cex/okx/okx.service';
 
 @Injectable()
 export class CexArbService implements OnModuleInit, OnModuleDestroy {
-  private isWatching = false;
+  private recentTicks: ITicker[] = [];
   private readonly logger = new Logger(CexArbService.name);
   private exchanges: Map<string, ccxt.Exchange> = new Map();
+  private isWatching = false;
 
   constructor(
     private binanceService: BinanceService,
-    private okxService: OkxService,
     private configService: ConfigService,
-    private pricesService: PricesService,
   ) {
     // Initialize exchanges from config
     const exchangesConfig = this.configService.get('exchanges');
@@ -27,7 +25,6 @@ export class CexArbService implements OnModuleInit, OnModuleDestroy {
           apiKey: exchange.apiKey,
           secret: exchange.apiSecret,
           enableRateLimit: true,
-          verbose: true,
         }),
       );
     }
@@ -46,18 +43,17 @@ export class CexArbService implements OnModuleInit, OnModuleDestroy {
 
   async startWatching() {
     this.isWatching = true;
-    // const symbol: string = this.configService.get('symbol');
+    const symbol: string = this.configService.get('symbol');
     // const symbols: string[] = this.configService.get('symbols');
     try {
       while (this.isWatching) {
-        // const listenTicker = await this.pricesService.fetchSingleTicker(this.exchanges, symbol);
-        const balanceResult = await this.binanceService.fetchBalance(['USDT', 'ETH']);
-        if (!balanceResult.success) {
-          console.error(`Failed to fetch balance: ${balanceResult.error}`);
-          continue;
-        }
-        this.logger.log('__balanceResult__', balanceResult);
-        await this.pricesService.delay();
+        await this.fetchSingleTicker(symbol);
+        // const balanceResult = await this.binanceService.fetchBalance(symbol);
+        // if (!balanceResult.success) {
+        //   console.error(`Failed to fetch balance: ${balanceResult.error}`);
+        //   continue;
+        // }
+        // this.logger.log('balanceResult: ', balanceResult);
       }
     } catch (error) {
       this.logger.error('Error in price watching loop:', error);
@@ -65,7 +61,146 @@ export class CexArbService implements OnModuleInit, OnModuleDestroy {
     }
   }
 
-  async stopWatching() {
+  private async fetchMultipleTickers(symbols: string[]): Promise<ITickerRecords[]> {
+    const tickerPromises = Array.from(this.exchanges.entries()).map(
+      async ([exchangeName, exchange]): Promise<IMultiTickers> => {
+        try {
+          const tickers = await exchange.fetchTickers(symbols);
+          if (!tickers) {
+            this.logger.warn(`No valid ticker data on ${exchangeName}`);
+            return null;
+          }
+          const tickData: IMultiTickers = {
+            exchange: exchangeName,
+            tickers,
+          };
+          // this.logger.log('__tickData: ', tickData);
+          // this.storeTickData(tickData);
+          // this.logger.log(`${symbol}: ${ticker.last} : ${exchangeName}`);
+          return tickData;
+        } catch (error) {
+          this.logger.error(`Error fetching ticker from ${exchangeName}: ${error.message}`);
+          return null;
+        }
+      },
+    );
+
+    const results = await Promise.all(tickerPromises);
+    const fulfillResults = results.filter((result): result is IMultiTickers => result !== null);
+    const validResults: ITickerRecords[] = symbols.map((symbol) => ({
+      [symbol]: fulfillResults.map((result) => ({
+        exchange: result.exchange,
+        ticker: result.tickers[symbol],
+        timestamp: result.tickers[symbol].timestamp,
+        last: result.tickers[symbol].last,
+      })),
+    }));
+    // this.logger.log('__validResults: ', validResults);
+    if (validResults.length > 0) {
+      validResults.map((validResult) => {
+        Object.values(validResult).map((results) => {
+          this.analyzePrices(results);
+        });
+      });
+    } else {
+      this.logger.warn('No valid ticker data received from any exchange');
+    }
+    await this.delay();
+    return validResults;
+  }
+
+  private async fetchSingleTicker(symbol: string): Promise<ITicker[]> {
+    const tickerPromises = Array.from(this.exchanges.entries()).map(
+      async ([exchangeName, exchange]): Promise<ITicker> => {
+        try {
+          const ticker = await exchange.fetchTicker(symbol);
+          if (!ticker || !ticker.last) {
+            this.logger.warn(`No valid ticker data for ${symbol} on ${exchangeName}`);
+            return null;
+          }
+          const tickData: ITicker = {
+            exchange: exchangeName,
+            ticker: ticker,
+            timestamp: ticker.timestamp,
+            last: ticker.last,
+          };
+          // this.storeTickData(tickData);
+          // this.logger.log(`${symbol}: ${ticker.last} : ${exchangeName}`);
+          return tickData;
+        } catch (error) {
+          this.logger.error(`Error fetching ${symbol} ticker from ${exchangeName}: ${error.message}`);
+          return null;
+        }
+      },
+    );
+
+    const results = await Promise.all(tickerPromises);
+    const validResults = results.filter((result): result is ITicker => result !== null);
+    // this.logger.log('validResults: ', validResults);
+    if (validResults.length > 0) {
+      this.analyzePrices(validResults);
+    } else {
+      this.logger.warn('No valid ticker data received from any exchange');
+    }
+    await this.delay();
+    return validResults;
+  }
+
+  private storeTickData(tickData: ITicker): void {
+    this.recentTicks.push(tickData);
+    if (this.recentTicks.length > 100) {
+      this.recentTicks.shift();
+    }
+  }
+
+  private async delay(): Promise<void> {
+    const delay_min: number = this.configService.get('fetch_delay_min');
+    const delay_max: number = this.configService.get('fetch_delay_max');
+    const delay = Math.floor(Math.random() * (delay_max - delay_min)) + delay_min;
+    this.logger.log(`____________________________________delay: ${delay}`);
+    await new Promise((resolve) => setTimeout(resolve, delay));
+  }
+  private analyzePrices(results: ITicker[]) {
+    const priceEntries = results
+      .filter((result) => result.ticker?.last)
+      .map((result) => [result.exchange, result.ticker.symbol, result.ticker.last] as const);
+
+    if (priceEntries.length >= 2) {
+      const { symbol, minExchange, minPrice, maxExchange, maxPrice } = priceEntries.reduce(
+        (acc, [exchange, symbol, price]) => ({
+          symbol: symbol,
+          minExchange: price < acc.minPrice ? exchange : acc.minExchange,
+          minPrice: Math.min(price, acc.minPrice),
+          maxExchange: price > acc.maxPrice ? exchange : acc.maxExchange,
+          maxPrice: Math.max(price, acc.maxPrice),
+        }),
+        {
+          symbol: priceEntries[0][1],
+          minExchange: priceEntries[0][0],
+          minPrice: priceEntries[0][2],
+          maxExchange: priceEntries[0][0],
+          maxPrice: priceEntries[0][2],
+        },
+      );
+
+      const priceDiff = maxPrice - minPrice;
+      const diffPercentage = (priceDiff / minPrice) * 100;
+
+      this.logger.log(` ${symbol}: Min: (${minPrice}) (${minExchange}) | Max: (${maxPrice}) (${maxExchange})`);
+      this.logger.log(`Price difference opportunity: ${priceDiff} (${diffPercentage.toFixed(4)}%)`);
+
+      const configuredDiff = this.configService.get('usdt_price_diff');
+
+      if (priceDiff > configuredDiff) {
+        // this.logger.log(`Have an opportunity!!!`);
+      }
+    }
+  }
+
+  stopWatching() {
     this.isWatching = false;
+  }
+  public getRecentTicks(): ITicker[] {
+    return this.recentTicks;
   }
 }
